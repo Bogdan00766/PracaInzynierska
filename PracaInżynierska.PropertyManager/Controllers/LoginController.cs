5452a1a9cf5eafffdfbf7d755d4b153593a92742
Piotr "Bogdan" Chudzik-Lipka <p.chudziklipka0@gmail.com>
2022-08-18 16:08:01 +0200

    Initial Commit

---
+++ PracaInżynierska.PropertyManager/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using PracaInżynierska.Domain.Models;
+
+namespace PracaInżynierska.PropertyManager.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class LoginController : Controller
+    {
+        
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+
+            return Ok();
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+
+            return Ok();
+        }
+
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+
+            return Ok();
+        }
+    }
+}
--- PracaInżynierska.PropertyManager/Program.cs
+++ PracaInżynierska.PropertyManager/Program.cs
@@ -5,12 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -18,8 +22,6 @@ app.UseStaticFiles(new StaticFileOptions()
 {
     OnPrepareResponse = (context) =>
     {
-        
-        // Retrieve cache configuration from appsettings.json
         context.Context.Response.Headers["Cache-Control"] = app.Configuration["StaticFiles:Headers:Cache-Control"];
         context.Context.Response.Headers["Pragma"] = app.Configuration["StaticFiles:Headers:Pragma"];
         context.Context.Response.Headers["Expires"] = app.Configuration["StaticFiles:Headers:Expires"];
