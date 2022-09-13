0310a8d77bfeddaab8c18da73b20e62af868ab0e
Piotr "Bogdan" Chudzik-Lipka <p.chudziklipka0@gmail.com>
2022-09-13 15:19:48 +0200

    Cretaed register.component

--- PracaInżynierska.PropertyManager/ClientApp/src/app/app.module.ts
+++ PracaInżynierska.PropertyManager/ClientApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
